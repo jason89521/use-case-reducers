3ac9e414594f1cebfd56434bb637838f94184357
Xuan <97ssps30212@gmail.com>
2022-04-26 22:09:48 +0800

    Add overload with third parameter

--- dev/App.tsx
+++ dev/App.tsx
@@ -1,19 +1,44 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import useCaseReducer from '../src';
 
+const init = (count: number) => {
+  console.log(count);
+  return count;
+};
+
 function App() {
-  const [count, dispatch] = useCaseReducer(0, {
-    increment: state => state + 1,
-    add: (state, payload: number) => state + payload,
-  });
+  const [state, dispatch] = useCaseReducer(
+    {
+      increment: state => state + 1,
+      add: (state, payload: number) => state + payload,
+    },
+    0,
+    init
+  );
+
+  const [rerenderCounts, setRerenderCounts] = useState({ state: 0, dispatch: 0 });
+
+  useEffect(() => {
+    setRerenderCounts(old => ({ ...old, state: old.state + 1 }));
+  }, [state]);
+
+  useEffect(() => {
+    setRerenderCounts(old => ({ ...old, dispatch: old.dispatch + 1 }));
+  }, [dispatch]);
 
   return (
-    <div>
-      <div>{count}</div>
+    <div style={{ display: 'flex', justifyContent: 'center', marginTop: '50px' }}>
+      <div>
+        <div>state: {state}</div>
 
-      <button onClick={() => dispatch.increment()}>increment</button>
-      <button onClick={() => dispatch.add(10)}>add 10</button>
+        <button onClick={() => dispatch.increment()}>increment</button>
+        <button onClick={() => dispatch.add(10)}>add 10</button>
+        <div>
+          <div>state re-render counts: {rerenderCounts.state}</div>
+          <div>dispatch re-render counts: {rerenderCounts.dispatch}</div>
+        </div>
+      </div>
     </div>
   );
 }
--- src/useCaseReducer.ts
+++ src/useCaseReducer.ts
@@ -6,13 +6,27 @@ import createReducer from './createReducer';
 import type { DispatchFromCRs } from './createDispatch';
 import createDispatch from './createDispatch';
 
-export default function useCaseReducer<State, CRs extends CaseReducers<State>>(
-  initialArg: State,
-  caseReducers: CRs
+function useCaseReducer<State, CRs extends CaseReducers<State>>(
+  caseReducers: CRs,
+  initialState: State
+): [State, DispatchFromCRs<State, CRs>];
+
+function useCaseReducer<State, CRs extends CaseReducers<State>, Arg>(
+  caseReducers: CRs,
+  initialState: Arg,
+  init: (arg: Arg) => State
+): [State, DispatchFromCRs<State, CRs>];
+
+function useCaseReducer<State, CRs extends CaseReducers<State>, Arg>(
+  caseReducers: CRs,
+  initialState: State | Arg,
+  init?: (arg: Arg) => State
 ): [State, DispatchFromCRs<State, CRs>] {
   const reducer = useRef(createReducer(caseReducers)).current;
-  const [state, reactDispatch] = useReducer(reducer, initialArg);
+  const [state, reactDispatch] = useReducer(reducer, initialState as any, init as any);
   const dispatch = useRef(createDispatch<State, CRs>(reactDispatch, caseReducers)).current;
 
   return [state, dispatch];
 }
+
+export default useCaseReducer;
