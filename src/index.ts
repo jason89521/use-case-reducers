6de2232e2397802342f39b5b74edb9e27db0589f
Xuan <97ssps30212@gmail.com>
2022-05-01 23:04:34 +0800

    Re-export type

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
-import useCaseReducers, { dispatch } from './useCaseReducers';
+import useCaseReducers from './useCaseReducers';
 import createCaseReducers from './createCaseReducers';
 import createActions from './createActions';
 import createReducer from './createReducer';
 import createSlice from './createSlice';
 
+import type { Action, ActionCreator, ActionFromCR, ActionsFromCRs } from './createActions';
+import type { CaseReducer, CaseReducers } from './createCaseReducers';
+
 export default useCaseReducers;
-export { dispatch, createCaseReducers, createActions, createReducer, createSlice };
+export { createCaseReducers, createActions, createReducer, createSlice };
+export type { Action, ActionCreator, ActionFromCR, ActionsFromCRs, CaseReducer, CaseReducers };
